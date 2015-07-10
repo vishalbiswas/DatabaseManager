ac285d91d5e60d53afa47829f13db5879e2d1e0a
vishalbiswas <vshlbiswas@ymail.com>
2015-07-10 09:28:05 +0530

    ClassicSwitcher class complete

---
+++ ClassicSwitcher/ClassicSwitcher/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace ClassicSwitcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Type> list = Assembly.GetExecutingAssembly().GetTypes().ToList();
+            string thisName = MethodBase.GetCurrentMethod().DeclaringType.Name;
+            int assemblyNameSize = Assembly.GetExecutingAssembly().GetName().Name.Length;
+
+            for (int i = 0; i < list.Count; ++i)
+            {
+                if (list[i].Name == thisName)
+                {
+                    list.RemoveAt(i);
+                    if (i == list.Count - 1) break;
+                }
+                Console.WriteLine(list[i].ToString().Substring(assemblyNameSize + 1));
+            }
+
+            Console.Write("Launch the App: ");
+            short choice = Convert.ToInt16(Console.ReadLine());
+            if (choice > 0 && choice <= list.Count) {
+                Console.Clear();
+                object instance = Activator.CreateInstance(list[choice + 1]);
+            }
+        }
+    }
+}
---
+++ ClassicSwitcher/ClassicSwitcher/TempOne.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassicSwitcher
+{
+    class TempOne
+    {
+        public TempOne()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
